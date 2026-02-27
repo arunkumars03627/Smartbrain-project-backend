01d84023d65c7f414178c562e80557acfe362bf4
Arunkumar <arunkumars03627@gmail.com>
2026-02-27 15:54:17 +0530

    updateport

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const port = process.env.PORT || 4000
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -28,13 +29,13 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.get('/', (req, res)=> { res.send('its working') })
+app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log('app is running on port ${process.env.PORT}');
+app.listen(port, ()=> {
+  console.log('app is running on port ${port}')
 })
